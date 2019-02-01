0f451d03532a3d59b943c402a449dc21399090d7
annelizabath <33921380+annelizabath@users.noreply.github.com>
2019-02-01 15:40:45 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LekshmiComponent } from './lekshmi/lekshmi.component';
+import { HelloworldComponent } from './helloworld/helloworld.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LekshmiComponent,
+    HelloworldComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
